0ddf1c16b2be9b1899ad780d98c809ce03865066
Baolin Zhu <zhubaolin228@gmail.com>
2026-02-19 15:20:17 +0800

    feat(packer): refactor Packer scripts and update Tekton pipeline for cloud-init and customize builds

---
+++ packer/ubuntu/packer-customize.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xeou pipefail
+
+export PACKER_LOG=1
+export PACKER_LOG_PATH="packer.log"
+
+packer build \
+    -on-error=abort \
+    -only="customize.qemu.ubuntu" .
+if [ -f output-customize/customize.qcow2 ]; then
+    echo "Output info:"
+    qemu-img info output-customize/customize.qcow2
+fi
