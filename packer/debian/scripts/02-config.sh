e8883a1f2529d9c92a6b9bc300fc4d0b3dc39650
eWloYW8 <3171132517@qq.com>
2026-03-19 12:21:33 +0800

    update(packer): add disable-llmnr configuration for systemd resolver

--- packer/debian/scripts/02-config.sh
+++ packer/debian/scripts/02-config.sh
@@ -26,6 +26,7 @@ install -D -m 0644 -o root -g root /tmp/rootfs/etc/systemd/system/docker.socket.
 install -D -m 0644 -o root -g root /tmp/rootfs/etc/systemd/system/mount-local.service /etc/systemd/system/mount-local.service
 install -D -m 0644 -o root -g root /tmp/rootfs/etc/systemd/system/otelcol-contrib.service.d/override.conf /etc/systemd/system/otelcol-contrib.service.d/override.conf
 install -D -m 0644 -o root -g root /tmp/rootfs/etc/systemd/system/sssd.service.d/override.conf /etc/systemd/systemd/otelcol-contrib.service.d/override.conf
+install -D -m 0644 -o root -g root /tmp/rootfs/etc/systemd/resolved.conf.d/disable-llmnr.conf /etc/systemd/resolved.conf.d/disable-llmnr.conf
 
 # sudoers (must be 0440)
 install -D -m 0440 -o root -g root /tmp/rootfs/etc/sudoers.d/audit /etc/sudoers.d/audit
